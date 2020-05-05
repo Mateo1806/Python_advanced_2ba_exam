9e66c60fdcc1c11aaea6801653cb122345c3a531
Matéo Yerlès <mathepirat1806@gmail.com>
2020-05-05 13:17:47 +0200

    Initial Commit Ended

--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@
 # Author: Sébastien Combéfis
 # Version: February 8, 2018
 
+from math import sqrt
+
 def fact(n):
 	"""Computes the factorial of a natural number.
 	
@@ -10,7 +12,11 @@ def fact(n):
 	Post: Returns the factorial of 'n'.
 	Throws: ValueError if n < 0
 	"""
-	pass
+	if n < 2:
+		return 1
+	else :
+		return n*fact(n-1)
+	
 
 def roots(a, b, c):
 	"""Computes the roots of the ax^2 + bx + x = 0 polynomial.
@@ -19,7 +25,13 @@ def roots(a, b, c):
 	Post: Returns a tuple with zero, one or two elements corresponding
 		to the roots of the ax^2 + bx + c polynomial.
 	"""
-	pass
+	d = b**2 - 4*a*c
+	if d < 0 :
+		return ()
+	if d == 0:
+		return ((-b )/(2*a),) #virgule après la parenthèse sinon il considère comme un nombre et pas comme un tuple
+	if d > 0 :
+		return ((-b + sqrt(d))/(2*a), (-b - sqrt(d))/(2*a))
 
 def integrate(function, lower, upper):
 	"""Approximates the integral of a fonction between two bounds
@@ -34,7 +46,17 @@ def integrate(function, lower, upper):
 		you'll probably need the 'eval' function to evaluate the function
 		to integrate given as a string.
 	"""
-	pass
+	x = 1
+	try:
+		_ = eval (function) #_ c'est une variable qui n'est pas utilisée
+	except:
+		"error in fuction"
+	if upper < lower:
+		return "upper is smaller than lower bound"
+	from scipy import integrate
+	fct = lambda x: eval(function)
+	result = integrate.quad(fct, lower, upper)
+	return result[0]
 
 if __name__ == '__main__':
 	print(fact(5))
